24a1878c1f726f151baf51e2026b5a3c98179d0e
bovojon <bovojon@gmail.com>
2017-01-09 15:54:55 -0500

    Change data

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# angular-tables
+## Description
+Simple AngularJS table based on the [tutorial from Scotch.io](https://scotch.io/tutorials/sort-and-filter-a-table-using-angular).
+
+## Technical Overview
+* AngularJS 1.x
+
+## Getting Started
+To get started, clone the repository, and run `http-server`.
+
+```shell
+git clone <this repo>
+cd <this project folder>
+http-server
+```
+
+This will start a web server on port 8080.
+
+
+[LIVE EXAMPLE](https://bovojon.github.io/angular-tables/#/)
--- app.js
+++ app.js
@@ -8,17 +8,17 @@
 
   TableController.$inject = ["$scope"];
   function TableController($scope) {
-    $scope.sortType = 'name';          // set the default sort type
+    $scope.sortType = 'title';          // set the default sort type
     $scope.sortReverse = false        // set the default sort order
-    $scope.searchFish = '';           // set the default search/filter term
+    $scope.searchTitle = '';           // set the default search/filter term
 
 
     // create list of sushi
-    $scope.sushi = [
-      { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-      { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-      { name: 'Tiger', fish: 'Eel', tastiness: 7 },
-      { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+    $scope.titles = [
+      { title: 'title-one', length: 'ten', month: 2 },
+      { title: 'title-two', length: 'five', month: 4 },
+      { title: 'title-three', length: 'nine', month: 7 },
+      { title: 'title-four', length: 'eight', month: 6 }
     ];
 
   }
